e4cc2413809d03a30213bdc7dc5bfd98e3310eb3
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2021-10-25 16:39:44 -0500

    Styling + annotation functionality

--- project-2/script.js
+++ project-2/script.js
@@ -12,27 +12,59 @@ $("#lines").scroll(function () {
   $("#col-rt").scrollTop($("#lines").scrollTop());
 });
 
-/* hover connections */
+/* hover + click connections */
 $(document).ready(function () {
-  $("#hl-one").hover(
+  // "updatee" annotation
+  $("#hl-update").click(
     function () {
-      $(".material").css("background-color", "#547655");
-      $("#material-ann").css("color", "white");
-    },
-    function () {
-      $(".material").css("background-color", "rgba(0,0,0,0)");
-      $("#material-ann").css("color", "rgba(0,0,0,0)");
-    },
-  );
-  $("#hl-two").hover(
-    function () {
-      $(".drugs").css("background-color", "#547655");
-    },
-    function () {
-      $(".drugs").css("background-color", "rgba(0,0,0,0)");
+      $("#update-ann").toggleClass("click-white");
+      $("#hl-update").toggleClass("highlight-clicked")
     }
   );
+  $("#hl-update").hover(
+    function () {
+      $("#update-ann").toggleClass("hvr-white");
+    }
+  );
+  // "universal" annotation
+  $("#hl-universal").click(
+    function () {
+      $("#univ-ann").toggleClass("click-white");
+      $("#hl-universal").toggleClass("highlight-clicked")
+    }
+  );
+  $("#hl-universal").hover(
+    function () {
+      $("#univ-ann").toggleClass("hvr-white");
+    }
+  );
+  // "swiss" annotation
+  $("#hl-swiss").click(
+    function () {
+      $("#swiss-ann").toggleClass("click-white");
+      $("#hl-swiss").toggleClass("highlight-clicked")
+    }
+  );
+  $("#hl-swiss").hover(
+    function () {
+      $("#swiss-ann").toggleClass("hvr-white");
+    }
+  );
+  // "brutal" annotation
+  $("#hl-brutal").click(
+    function () {
+      $("#brutal-ann").toggleClass("click-white");
+      $("#hl-brutal").toggleClass("highlight-clicked")
+    }
+  );
+  $("#hl-swiss").hover(
+    function () {
+      $("#brutal-ann").toggleClass("hvr-white");
+    }
+  );
+
 });
+
 
 var step = 200;
 var scrolling = false;
@@ -52,6 +84,18 @@ $("#scrollDown").bind("click", function (event) {
   $("#col-lft").animate({
     scrollTop: "+=" + step + "px",
   });
+});
+
+$(document).keydown(function (evt) {
+  if (evt.keyCode == 40) { // down arrow
+    $("#col-lft").animate({
+      scrollTop: "+=" + step + "px",
+    });
+  } else if (evt.keyCode == 38) {
+    $("#col-lft").animate({
+      scrollTop: "-=" + step + "px",
+    });
+  }
 });
 
 function scrollContent(direction) {
